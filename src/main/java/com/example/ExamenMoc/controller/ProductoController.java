3cb6e17360f947f437b6dcf0ec2fc8eda19a9d0b
jesusdd36 <jesusdd36@educastur.es>
2025-11-21 18:55:42 +0100

    Realizado el Controller

---
+++ src/main/java/com/example/ExamenMoc/controller/ProductoController.java
@@ -0,0 +1,48 @@
+package com.example.ExamenMoc.controller;
+
+import com.example.ExamenMoc.entity.Producto;
+import com.example.ExamenMoc.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @PostMapping(value = "/productos")
+    public Producto addProducto(@RequestBody Producto producto) {
+        return this.productoService.addProducto(producto);
+    }
+
+    @DeleteMapping(value = "/producto/{productoId}")
+    public void deleteProducto(@PathVariable("productoId") Long productoId) {
+        this.productoService.eliminarProductoById(productoId);
+    }
+
+    @PutMapping(value = "/producto/{productoId}")
+    public Producto modificarProducto(@PathVariable Long productoId, @RequestBody Producto producto) {
+        return this.productoService.modificarProducto(productoId,producto);
+    }
+
+    @GetMapping(value = "/productos")
+    public List<Producto> getProductos(@RequestParam(defaultValue = "0.0") Float precio,
+                                       @RequestParam(defaultValue = "") String categoria) {
+
+
+        this.productoService.findAllProductos();
+        this.productoService.findByPrecio(precio);
+        return productoService.findByCategoria(categoria);
+
+    }
+
+    @GetMapping(value = "/producto/{productoId}")
+    public Optional<Producto> getProducto(@PathVariable Long productoId) {
+        return this.productoService.findProducto(productoId);
+
+    }
+
+}
